0c6d69383b93209566d9af9eee7e761c861e06f4
seungbum <tmdqja3377@cu.ac.kr>
2023-09-20 02:57:36 +0000

    sas

---
+++ chap2/prop4.py
@@ -0,0 +1,4 @@
+print("Game Over\n\n")
+
+
+print("Press the enter key to exit.")
---
+++ chap2/prop61.py
@@ -0,0 +1,13 @@
+print("\n           Trust Fund Buddy\n\n")
+print("Total your monthly spending so that your trust fund doesn't run out")
+print("<and you're forced to get a real job>.\n\n")
+print("please enter the requested, monthly costs.   Since you're rich, ignore pennies")
+print("and use only dollar amounts.")
+print("\n\n")
+print("Lamborghini Tune-Ups:", end='')
+var = int(input())
+var = var + int(input("manhattan Apartment:"))
+var = var + int(input("Private Jet Rental:"))
+var = var + int(input("Gifts:"))
+
+
