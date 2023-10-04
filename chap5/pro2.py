62fdbced191d9142ab1494ecaa37bb35e2d9d170
seungbum <tmdqja3377@cu.ac.kr>
2023-10-04 03:00:01 +0000

    week5

---
+++ chap5/pro2.py
@@ -0,0 +1,7 @@
+import random
+
+game = ["difficult","banana","apple","python","daegu"]
+print("Welcome to Wor jumble!")
+print("Unscramble the letters to make word.")
+l = random.shuffle(game)
+print("Jumbled word: ",l)
